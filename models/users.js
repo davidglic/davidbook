f45ba51195e28049dcdfafba370a3a520a88dbae
David Lewis <david_lewis@glic.com>
2021-05-14 09:31:36 -0700

    users model added

---
+++ models/users.js
@@ -0,0 +1,13 @@
+const users = [
+    {
+        username: 'david',
+        email: 'david@david.com',
+        password: 'password',
+        aboutMe: "Yada yada yada...",
+        img: 'https://i1.wp.com/www.tor.com/wp-content/uploads/2018/02/Spirit2008-01.jpg',
+        uuid: 1112
+
+    }
+]
+
+module.exports = users
